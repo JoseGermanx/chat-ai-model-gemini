c4ca1c38319153278183d2e002685642814fa0a3
Jose Germán Martínez <jgermanmm@gmail.com>
2024-07-01 13:08:36 -0400

    `chore: Update Switch component styles for width and background image`

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -2,15 +2,11 @@
 import { useTheme } from '../../hooks/useTheme';
 import './Switch.css'
 
-import sun from '../../assets/sun-svgrepo-com.svg';
-import moon from '../../assets/moon-svgrepo-com.svg';
-
 const Switch = () => {
     const [theme, handleChange] = useTheme('light');
 
     return (
         <div className="container-switch">
-            <span style={{fontSize: "8px"}}>{theme === "dark" ? <img src={sun} width={30} alt="Ligth" /> : <img src={moon} width={30} alt="Dark" />}</span>
             <label className="switch">
                 <input type="checkbox" onChange={handleChange} checked={theme === 'dark'} />
                 <span className="slider"></span>
